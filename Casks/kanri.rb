e8421f1436c4075f3a28e5628193f34b78ccca13
trobo <57040351+trobonox@users.noreply.github.com>
2024-06-24 11:57:30 +0200

    Update kanri.rb

--- Casks/kanri.rb
+++ Casks/kanri.rb
@@ -1,9 +1,9 @@
 cask "kanri" do
     arch arm: "aarch64", intel: "x64"
   
-    version "0.7.0"
-    sha256 arm:   "5a9e5e76f230c0e66514a95f23e0591fee63b58a56ed3ef987c1915ef1e6584a",
-           intel: "ebeebafd74dd3a74459b4ce0f952d21293cb107161a51728ff1a093f8f3b4de8"
+    version "0.7.1"
+    sha256 arm:   "7e144746edcb304238f2e8e6f1518fae47e987c181aee57d43411d78341e027b",
+           intel: "d09b7b724109e1f5b3325a554cb403838d6ee112d863a7ceb9deb3890c266f01"
   
     url "https://github.com/trobonox/kanri/releases/download/app-v#{version}/kanri_#{version}_#{arch}.dmg"
     name "Kanri"
